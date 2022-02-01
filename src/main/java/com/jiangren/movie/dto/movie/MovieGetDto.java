adb51c5d76ff63c294d28a73354f09619ab8c24d
Zhang-Haoran <haoranzhang001@gmail.com>
2022-02-01 13:50:30 +1100

    updated Movie dto

--- src/main/java/com/jiangren/movie/dto/movie/MovieGetDto.java
+++ src/main/java/com/jiangren/movie/dto/movie/MovieGetDto.java
@@ -1,4 +1,24 @@
 package com.jiangren.movie.dto.movie;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
 public class MovieGetDto {
+    private Long id;
+
+    private String movie_name;
+
+    private String movie_lang;
+
+    private String age_certificate;
+
+    private Integer movie_length;
+
+    private LocalDate release_date;
+
 }
+
--- src/main/java/com/jiangren/movie/dto/movie/MoviePostDto.java
+++ src/main/java/com/jiangren/movie/dto/movie/MoviePostDto.java
@@ -1,4 +1,22 @@
 package com.jiangren.movie.dto.movie;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
 public class MoviePostDto {
+    private String movie_name;
+
+    private String movie_lang;
+
+    private String age_certificate;
+
+    private Integer movie_length;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate release_date;
 }
