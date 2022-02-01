dc24afdbc1009e6c711e7885f82b436c329c9a70
Zhang-Haoran <haoranzhang001@gmail.com>
2022-02-01 18:27:47 +1100

    updated MovieRevenue

--- src/main/java/com/jiangren/movie/controller/MovieRevenueController.java
+++ src/main/java/com/jiangren/movie/controller/MovieRevenueController.java
@@ -1,4 +1,45 @@
 package com.jiangren.movie.controller;
 
+import com.jiangren.movie.dto.movieRevenue.MovieRevenueGetDto;
+import com.jiangren.movie.dto.movieRevenue.MovieRevenuePostDto;
+import com.jiangren.movie.dto.movieRevenue.MovieRevenuePutDto;
+import com.jiangren.movie.service.MovieRevenueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/movies-revenues")
+@RequiredArgsConstructor
 public class MovieRevenueController {
+    private final MovieRevenueService movieRevenueService;
+
+    @GetMapping
+    public ResponseEntity<List<MovieRevenueGetDto>> find(@RequestParam(required = false) String firstname,
+                                                     @RequestParam(required = false) String lastname) {
+        return ResponseEntity.ok(movieRevenueService.getAllMovieRevenues());
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieRevenueGetDto> create(@RequestBody MovieRevenuePostDto movieRevenuePostDto) {
+        return ResponseEntity.ok(movieRevenueService.create(movieRevenuePostDto));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestParam Long id, @RequestBody MovieRevenuePutDto movieRevenuePutDto) {
+        if (movieRevenueService.getById(id).size() == 0) return ResponseEntity.status(404).body("Id not found");
+        return ResponseEntity.ok(movieRevenueService.update(id, movieRevenuePutDto));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestParam Long id) {
+        if (movieRevenueService.getById(id).size() == 0) return ResponseEntity.status(404).body("Id not found");
+        try {
+            movieRevenueService.delete(id);
+            return ResponseEntity.ok().body("Deleted");
+        } catch (Exception e) {
+            return ResponseEntity.status(422).body("Foreign key constraint");
+        }
+    }
 }
--- src/main/java/com/jiangren/movie/service/MovieRevenueService.java
+++ src/main/java/com/jiangren/movie/service/MovieRevenueService.java
@@ -1,4 +1,44 @@
 package com.jiangren.movie.service;
 
+import com.jiangren.movie.dao.MovieRevenueDao;
+import com.jiangren.movie.dto.movieRevenue.MovieRevenueGetDto;
+import com.jiangren.movie.dto.movieRevenue.MovieRevenuePostDto;
+import com.jiangren.movie.dto.movieRevenue.MovieRevenuePutDto;
+import com.jiangren.movie.entity.MovieRevenue;
+import com.jiangren.movie.mapper.MovieRevenueMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+@RequiredArgsConstructor
 public class MovieRevenueService {
+    private final MovieRevenueDao movieRevenueDao;
+
+    private final MovieRevenueMapper movieRevenueMapper;
+
+    public List<MovieRevenueGetDto> getAllMovieRevenues(){
+        return movieRevenueDao.findAllByOrderByIdDesc().stream().map(movieRevenueMapper::fromEntity).collect(Collectors.toList());
+    }
+
+    public MovieRevenueGetDto create(MovieRevenuePostDto movieRevenuePostDto){
+        MovieRevenue movieRevenue = movieRevenueMapper.toEntity(movieRevenuePostDto);
+        return movieRevenueMapper.fromEntity(movieRevenueDao.save(movieRevenue));
+    }
+
+    public MovieRevenueGetDto update(Long id, MovieRevenuePutDto movieRevenuePutDto){
+        MovieRevenue movieRevenue = new MovieRevenue();
+        movieRevenueMapper.copy(movieRevenuePutDto, movieRevenue);
+        movieRevenue.setId(id);
+        return movieRevenueMapper.fromEntity(movieRevenueDao.save(movieRevenue));
+    }
+
+    public void delete(Long id){
+        movieRevenueDao.deleteById(id);
+    }
+
+    public List<MovieRevenueGetDto> getById(Long id){
+        return movieRevenueDao.findById(id).stream().map(movieRevenueMapper::fromEntity).collect(Collectors.toList());
+    }
 }
