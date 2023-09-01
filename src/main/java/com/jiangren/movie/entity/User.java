8fa05d42cf81170f811f143a34a0ce97e36c0bd1
Zhang-Haoran <haoranzhang001@gmail.com>
2023-09-01 14:40:50 +1000

    fix issue

--- src/main/java/com/jiangren/movie/entity/User.java
+++ src/main/java/com/jiangren/movie/entity/User.java
@@ -10,16 +10,17 @@ import java.time.OffsetDateTime;
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
-    @Column(unique = true, nullable = false)
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
-    @Column(nullable = false)
+    @Column(name = "name",nullable = false)
     private String name;
-    @Column(nullable = false)
+    @Column(name = "password",nullable = false)
     private String password;
     @CreationTimestamp
     private OffsetDateTime createdTime;
